e9b92597278d193e85f099ffcf26aa9651aa11b0
= <martinkalvin6@gmail.com>
2021-08-04 18:16:40 +0100

    Added more checks

--- Mem/Entities.go
+++ Mem/Entities.go
@@ -3,12 +3,15 @@ package mem
 import "unsafe"
 
 const (
-	dwLocalPlayer uint32 = 0xD8A2DC
-	dwEntityList  uint32 = 0x4DA31DC
-	m_iTeamNum    uint32 = 0xF4
-	m_bSpotted    uint32 = 0x93D
-	m_bDormant    uint32 = 0xED
-	m_iHealth     uint32 = 0x100
+	dwLocalPlayer                   uint32 = 0xD8A2DC
+	dwEntityList                    uint32 = 0x4DA31DC
+	m_iTeamNum                      uint32 = 0xF4
+	m_bSpotted                      uint32 = 0x93D
+	m_bDormant                      uint32 = 0xED
+	m_iHealth                       uint32 = 0x100
+	m_bGunGameImmunity              uint32 = 0x3944
+	dwGameRulesProxy                uint32 = 0x52C02BC
+	m_SurvivalGameRuleDecisionTypes uint32 = 0x1328
 )
 
 func EntityFromId(id uint32) uint32 {
@@ -53,4 +56,39 @@ func SetSpotted(dwEnt uint32, bIsSpotted bool) {
 
 func IsAlive(dwEnt uint32) bool {
 	return Health(dwEnt) > 0
+}
+
+func HasGunGameImmunity(dwEnt uint32) bool {
+	var bHasGunGameImmunity bool
+	RPM(dwEnt+m_bGunGameImmunity, uintptr(unsafe.Pointer(&bHasGunGameImmunity)), unsafe.Sizeof(bHasGunGameImmunity))
+	return bHasGunGameImmunity
+}
+
+func IsDangerZone() bool {
+	var dwGameRules uint32
+	RPM(MemManager().Client+dwGameRulesProxy, uintptr(unsafe.Pointer(&dwGameRules)), unsafe.Sizeof(dwGameRules))
+	var iSurvivalGRDT int
+	RPM(dwGameRules+m_SurvivalGameRuleDecisionTypes, uintptr(unsafe.Pointer(&iSurvivalGRDT)), unsafe.Sizeof(iSurvivalGRDT))
+	return (iSurvivalGRDT != 0)
+}
+
+func IsValidTarget(dwEnt uint32, bGunGameCheck bool) bool {
+
+	if IsDormant(dwEnt) {
+		return false
+	}
+
+	if !IsAlive(dwEnt) {
+		return false
+	}
+
+	if Team(dwEnt) <= 1 || Team(dwEnt) == Team(LocalPlayer()) && !IsDangerZone() {
+		return false
+	}
+
+	if HasGunGameImmunity(dwEnt) && bGunGameCheck {
+		return false
+	}
+
+	return true
 }
